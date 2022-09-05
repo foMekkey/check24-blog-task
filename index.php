daa646411b4d0e046d634691f5df8890f48ef018
fouad mekkey <codepropcmaster@gmail.com>
2022-09-05 15:59:13 +0200

    create views and routes , install simple php router

--- index.php
+++ index.php
@@ -4,6 +4,16 @@ declare(strict_types=1);
 
 error_reporting(E_ALL);
 
+use Steampixel\Route;
+
 require_once realpath(__DIR__ . '/vendor/autoload.php');
 
+$cur_dir = explode('\\', __DIR__);
+
+$actual_link = env('APP_URL');
+$curPageURL = 'http://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
+
 define('VIEWS', __DIR__ . '/views/');
+
+require_once realpath(__DIR__ . '/routes/routes.php');
+Route::run(BASEPATH);
